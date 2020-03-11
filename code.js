2ce206509ab23a0ce7f1d86c7c7e5d3781465220
Jonathan Fisher <jonathanfisher@missions-mbp.lan>
2020-03-11 08:17:46 +0000

    remove comment

--- code.js
+++ code.js
@@ -8,20 +8,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-/*
-const numberOfRectangles = 5
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
-*/
-//figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
 let dotSize = 1;
 let distance = 12;
 figma.ui.onmessage = (params) => __awaiter(this, void 0, void 0, function* () {
--- code.ts
+++ code.ts
@@ -1,20 +1,5 @@
 //https://www.figma.com/plugin-docs/intro/
 
-/*
-const numberOfRectangles = 5
-const nodes: SceneNode[] = [];
-for (let i = 0; i < numberOfRectangles; i++) {
-  const rect = figma.createRectangle();
-  rect.x = i * 150;
-  rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  figma.currentPage.appendChild(rect);
-  nodes.push(rect);
-}
-figma.currentPage.selection = nodes;
-figma.viewport.scrollAndZoomIntoView(nodes);
-*/
-
-//figma.viewport.scrollAndZoomIntoView(figma.currentPage.selection);
 let dotSize = 1;
 let distance = 12;
 figma.ui.onmessage = async(params) => {
